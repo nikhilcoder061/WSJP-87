dc4e2663a83a90da5741af54a59583d3ffe8caf2
Nikhil <nikhilcoder061@gmail.com>
2025-04-01 16:11:05 +0530

    tailwind day 1

---
+++ 5 MERN/react/tailwind/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className='text-[#1abc9c] bg-blue-300 hover:bg-blue-600 transition duration-1000 hover:text-red-600'>Welcome to WS</h1>
+  )
+}
