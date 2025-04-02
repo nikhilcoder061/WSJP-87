f0d19500e36f3c8301e90092bfe52817a42fadcc
Nikhil <nikhilcoder061@gmail.com>
2025-04-02 15:53:47 +0530

    tailwind day 2

--- 5 MERN/react/tailwind/src/App.jsx
+++ 5 MERN/react/tailwind/src/App.jsx
@@ -2,6 +2,39 @@ import React from 'react'
 
 export default function App() {
   return (
-    <h1 className='text-[#1abc9c] bg-blue-300 hover:bg-blue-600 transition duration-1000 hover:text-red-600'>Welcome to WS</h1>
+    <div className='w-screen'>
+      <div className='container bg-blue-100 p-2 mx-auto'>
+        <div className="grid xl:grid-cols-8 lg:grid-cols-4 md:grid-cols-2 gap-2 group">
+          <div className='bg-orange-300 group-hover:bg-green-300 p-3 transtion duration-1000'>
+            1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus est voluptas doloribus ut recusandae harum praesentium, ipsam libero, corporis nihil iure reiciendis consectetur deserunt, dolorum maxime corrupti animi eos?
+          </div>
+          <div className='bg-orange-300 p-3 animate-bounce '>
+            2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus est voluptas doloribus ut recusandae harum praesentium, ipsam libero, corporis nihil iure reiciendis consectetur deserunt, dolorum maxime corrupti animi eos?
+          </div>
+          <div className='bg-orange-300 p-3 select-none'>
+            3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus est voluptas doloribus ut recusandae harum praesentium, ipsam libero, corporis nihil iure reiciendis consectetur deserunt, dolorum maxime corrupti animi eos?
+          </div>
+          <div className='bg-orange-300 p-3 shadow-[0px_0px_5px_3px_red]'>
+            4. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus est voluptas doloribus ut recusandae harum praesentium, ipsam libero, corporis nihil iure reiciendis consectetur deserunt, dolorum maxime corrupti animi eos?
+          </div>
+          <div className='bg-orange-300 p-3'>
+            5. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus est voluptas doloribus ut recusandae harum praesentium, ipsam libero, corporis nihil iure reiciendis consectetur deserunt, dolorum maxime corrupti animi eos?
+          </div>
+          <div className='bg-orange-300 p-3 '>
+            6. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus est voluptas doloribus ut recusandae harum praesentium, ipsam libero, corporis nihil iure reiciendis consectetur deserunt, dolorum maxime corrupti animi eos?
+          </div>
+          <div className='bg-orange-300 p-3'>
+            7. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus est voluptas doloribus ut recusandae harum praesentium, ipsam libero, corporis nihil iure reiciendis consectetur deserunt, dolorum maxime corrupti animi eos?
+          </div>
+          <div className='bg-orange-300 p-3'>
+            8. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus est voluptas doloribus ut recusandae harum praesentium, ipsam libero, corporis nihil iure reiciendis consectetur deserunt, dolorum maxime corrupti animi eos?
+          </div>
+          {/* <div className='bg-orange-300 p-3'>
+            9. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus est voluptas doloribus ut recusandae harum praesentium, ipsam libero, corporis nihil iure reiciendis consectetur deserunt, dolorum maxime corrupti animi eos?
+          </div> */}
+
+        </div>
+      </div>
+    </div>
   )
 }
